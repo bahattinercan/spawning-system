a858f563163295af1b6a7f090069310fb8ca111c
ROWLEYES\donat <bahattin.ercan.43@gmail.com>
2022-11-12 13:47:45 +0300

    .13

--- Assets/Spawning System/Scripts/SpawningSystem.cs
+++ Assets/Spawning System/Scripts/SpawningSystem.cs
@@ -16,27 +16,36 @@ public struct SpawnPoint
     [Range(0, 100)] public int spawnPercent;
 }
 
-public enum ESpawnTypes
+public enum ESpawnType
 {
     normal,
-    withTarget
+    withTarget,
+    spawnAllSpawnPoints,
 }
 
 public class SpawningSystem : MonoBehaviour
-{    
+{
     public bool canSpawn;
     public bool activeDebugMessage;
+
+    [Space]
     [SerializeField] private List<SpawnObject> spawnObjects;
+
     [SerializeField] private List<SpawnPoint> spawnPoints;
+
+    [Space]
     [SerializeField] private Transform target;
+
     [SerializeField] private float startSpawnDelay, spawnDelay;
-    [SerializeField] private ESpawnTypes spawnTypes;
+    [SerializeField] private ESpawnType spawnType;
+
+    [Space]
     public UnityEvent onSpawned;
 
     private void Start()
     {
-        InvokeRepeating("Spawn", startSpawnDelay, spawnDelay);
         CheckForPercentages();
+        StartSpawn();
     }
 
     private void CheckForPercentages()
@@ -57,7 +66,7 @@ public class SpawningSystem : MonoBehaviour
         {
             totalSpawnPointPercentage += spawnPoints[i].spawnPercent;
         }
-        if (totalSpawnPointPercentage != 100)
+        if (totalSpawnPointPercentage != 100 && spawnType != ESpawnType.spawnAllSpawnPoints)
         {
             Debug.LogError("Total spawn point percentage is not 100. Total percentage : " + totalSpawnPointPercentage);
             Debug.LogError("Spawning System can't work correctly!!!");
@@ -70,87 +79,140 @@ public class SpawningSystem : MonoBehaviour
             Debug.Log(message);
     }
 
-    private void Spawn()
+    private void SpawnRandomPointAndRandomObject()
     {
-        if (canSpawn)
+        if (canSpawn == false)
+            return;
+        SpawnPoint randomSpawnPoint = new SpawnPoint();
+        SpawnObject randomSpawnObject = new SpawnObject();
+
+        int random = Random.Range(0, 101);
+        int lastTotalPercentage = 0;
+        int currentTotalPercentage = 0;
+        if (spawnPoints.Count == 1)
         {
-            SpawnPoint randomSpawnPoint = new SpawnPoint();
-            SpawnObject randomSpawnObject = new SpawnObject();
-
-            int random = Random.Range(0, 101);
-            int lastTotalPercentage = 0;
-            int currentTotalPercentage = 0;
-            if (spawnPoints.Count == 1)
+            randomSpawnPoint = spawnPoints[0];
+        }
+        else
+        {
+            for (int i = 0; i < spawnPoints.Count; i++)
             {
-                randomSpawnPoint = spawnPoints[0];
-            }
-            else
-            {
-                for (int i = 0; i < spawnPoints.Count; i++)
+                currentTotalPercentage = spawnPoints[i].spawnPercent + lastTotalPercentage;
+                if (random >= lastTotalPercentage && random <= currentTotalPercentage)
                 {
-                    currentTotalPercentage = spawnPoints[i].spawnPercent + lastTotalPercentage;
-                    if (random >= lastTotalPercentage && random <= currentTotalPercentage)
-                    {
-                        randomSpawnPoint = spawnPoints[i];
-                        WriteDebug("Random Spawn Point Index: " + i);
-                        break;
-                    }
-                    lastTotalPercentage += spawnPoints[i].spawnPercent;
+                    randomSpawnPoint = spawnPoints[i];
+                    WriteDebug("Random Spawn Point Index: " + i);
+                    break;
                 }
+                lastTotalPercentage += spawnPoints[i].spawnPercent;
             }
+        }
 
-            random = Random.Range(0, 101);
-            lastTotalPercentage = 0;
-            currentTotalPercentage = 0;
-            WriteDebug("random2:" + random);
-            if (spawnObjects.Count == 1)
+        random = Random.Range(0, 101);
+        lastTotalPercentage = 0;
+        currentTotalPercentage = 0;
+        WriteDebug("random2:" + random);
+        if (spawnObjects.Count == 1)
+        {
+            randomSpawnObject = spawnObjects[0];
+        }
+        else
+        {
+            for (int i = 0; i < spawnObjects.Count; i++)
             {
-                randomSpawnObject = spawnObjects[0];
-            }
-            else
-            {
-                for (int i = 0; i < spawnObjects.Count; i++)
+                currentTotalPercentage = spawnObjects[i].spawnPercent + lastTotalPercentage;
+                if (random >= lastTotalPercentage && random <= currentTotalPercentage)
                 {
-                    currentTotalPercentage = spawnObjects[i].spawnPercent + lastTotalPercentage;
-                    if (random >= lastTotalPercentage && random <= currentTotalPercentage)
-                    {
-                        randomSpawnObject = spawnObjects[i];
-                        WriteDebug("Random Spawn Object Index: " + i);
-                        break;
-                    }
-                    lastTotalPercentage += spawnObjects[i].spawnPercent;
+                    randomSpawnObject = spawnObjects[i];
+                    WriteDebug("Random Spawn Object Index: " + i);
+                    break;
                 }
+                lastTotalPercentage += spawnObjects[i].spawnPercent;
             }
+        }
 
-            Vector3 spawnPoint = Vector3.zero;
-            switch (spawnTypes)
-            {
-                case ESpawnTypes.normal:
-                    spawnPoint = randomSpawnPoint.spawningPoint.position;
-                    break;
+        Vector3 spawnPoint = Vector3.zero;
+        switch (spawnType)
+        {
+            case ESpawnType.normal:
+                spawnPoint = randomSpawnPoint.spawningPoint.position;
+                break;
 
-                case ESpawnTypes.withTarget:
+            case ESpawnType.withTarget:
 
-                    Vector3 tempTargetPos = new Vector3(target.position.x, 0, target.position.z);
-                    spawnPoint = randomSpawnPoint.spawningPoint.position + tempTargetPos;
-                    break;
-            }
+                Vector3 tempTargetPos = new Vector3(target.position.x, 0, target.position.z);
+                spawnPoint = randomSpawnPoint.spawningPoint.position + tempTargetPos;
+                break;
+        }
 
+        if (randomSpawnObject.prefab != null)
+        {
             Instantiate(randomSpawnObject.prefab, spawnPoint, Quaternion.identity);
             onSpawned?.Invoke();
             WriteDebug(randomSpawnObject.prefab.name + " named object spawned at " + randomSpawnPoint.spawningPoint.name);
         }
     }
 
-    public void StartSpawn()
+    private void SpawnInAllPointAndRandomObject()
     {
-        canSpawn = true;
-        InvokeRepeating("Spawn", startSpawnDelay, spawnDelay);
+        if (canSpawn == false)
+            return;
+
+        foreach (SpawnPoint itemSpawnPoint in spawnPoints)
+        {
+            if (Random.Range(0, 101) < itemSpawnPoint.spawnPercent)
+            {
+                SpawnObject randomSpawnObject = new SpawnObject();
+
+                int random = Random.Range(0, 101);
+                WriteDebug("random2:" + random);
+                if (spawnObjects.Count == 1)
+                {
+                    randomSpawnObject = spawnObjects[0];
+                }
+                else
+                {
+                    int lastTotalPercentage = 0;
+                    int currentTotalPercentage = 0;
+                    for (int i = 0; i < spawnObjects.Count; i++)
+                    {
+                        currentTotalPercentage = spawnObjects[i].spawnPercent + lastTotalPercentage;
+                        if (random >= lastTotalPercentage && random <= currentTotalPercentage)
+                        {
+                            randomSpawnObject = spawnObjects[i];
+                            WriteDebug("Random Spawn Object Index: " + i);
+                            break;
+                        }
+                        lastTotalPercentage += spawnObjects[i].spawnPercent;
+                    }
+                }
+
+                Vector3 spawnPoint = itemSpawnPoint.spawningPoint.position;
+
+                if (randomSpawnObject.prefab != null)
+                {
+                    Instantiate(randomSpawnObject.prefab, spawnPoint, Quaternion.identity);
+                    WriteDebug(randomSpawnObject.prefab.name + " named object spawned at " + itemSpawnPoint.spawningPoint.name);
+                }
+            }
+        }
+        onSpawned?.Invoke();
     }
 
-    public void StopSpawn() 
+    public void StartSpawn()
     {
-        canSpawn = false;
-        CancelInvoke("Spawn");
+        if (spawnType == ESpawnType.normal || spawnType == ESpawnType.withTarget)
+        {
+            InvokeRepeating("SpawnRandomPointAndRandomObject", startSpawnDelay, spawnDelay);
+        }
+        else
+        {
+            SpawnInAllPointAndRandomObject();
+        }
+    }
+
+    public void StopSpawn()
+    {
+        CancelInvoke();
     }
 }
